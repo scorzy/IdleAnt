94b35d32d0c1d459dc87779ba841ec4a1a3fe7ea
lorenzo <scorzy88@gmail.com>
2018-08-26 21:25:52 +0200

    Numberfomat #27

--- src/app/model/options.ts
+++ src/app/model/options.ts
@@ -1,39 +1,47 @@
-declare let numberformat
-declare let setCss: any
+declare let numberformat;
+declare let setCss: any;
 
 export class Options {
+  header = 5;
+  dark = false;
+  usaFormat = false;
+  numFormat = "T";
+  formatter = new numberformat.Formatter({
+    format: "standard",
+    flavor: "short"
+  });
+  width = 0;
+  height = 0;
 
-  header = 5
-  dark = false
-  usaFormat = false
-  numFormat = "T"
-  formatter = new numberformat.Formatter({ format: 'standard', sigfigs: 2, flavor: 'short' })
-  width = 0
-  height = 0
-
-  constructor(
-  ) { }
+  constructor() {
+    this.generateFormatter();
+  }
 
   apply() {
-    setCss(this.dark)
+    setCss(this.dark);
   }
   generateFormatter() {
     this.formatter = new numberformat.Formatter({
-      format: this.numFormat === "T" ? 'standard' : (
-        this.numFormat === "S" ? 'scientific' : (
-          this.numFormat === "E" ? 'engineering' : 'longScale'))
-      , sigfigs: 2, flavor: 'short'
-    })
+      format:
+        this.numFormat === "T"
+          ? "standard"
+          : this.numFormat === "S"
+            ? "scientific"
+            : this.numFormat === "E"
+              ? "engineering"
+              : "longScale",
+      sigfigs: 3,
+      flavor: "short"
+    });
   }
   load(data: any) {
-    if (data.header) this.header = data.header
-    if (data.dark) this.dark = data.dark
-    if (data.usaFormat) this.usaFormat = data.usaFormat
-    if (data.numFormat) this.numFormat = data.numFormat
-    if (data.width) this.width = data.width
-    if (data.height) this.height = data.height
+    if (data.header) this.header = data.header;
+    if (data.dark) this.dark = data.dark;
+    if (data.usaFormat) this.usaFormat = data.usaFormat;
+    if (data.numFormat) this.numFormat = data.numFormat;
+    if (data.width) this.width = data.width;
+    if (data.height) this.height = data.height;
 
-    this.generateFormatter()
+    this.generateFormatter();
   }
-
 }
