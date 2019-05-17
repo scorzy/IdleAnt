0c6a1c9e1bfe5609bd5af739d1baf9b4f54cc6ee
scorzy <scorzy@users.noreply.github.com>
2019-05-17 22:26:37 +0200

    localstorage

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,23 +1,22 @@
-import * as decimal from 'decimal.js'
-import { GameModel } from './model/gameModel'
-import { log } from 'util'
-import { Logger } from 'jasmine-spec-reporter/built/display/logger'
-import { Injectable, OnInit, ViewContainerRef } from '@angular/core'
-import { ActivatedRoute, Router } from '@angular/router'
-import { ToastsManager } from 'ng2-toastr/ng2-toastr'
-import * as moment from 'moment'
-import 'rxjs/add/observable/interval'
-import { Observable } from 'rxjs/Observable'
+import * as decimal from "decimal.js"
+import { GameModel } from "./model/gameModel"
+import { log } from "util"
+import { Logger } from "jasmine-spec-reporter/built/display/logger"
+import { Injectable, OnInit, ViewContainerRef } from "@angular/core"
+import { ActivatedRoute, Router } from "@angular/router"
+import { ToastsManager } from "ng2-toastr/ng2-toastr"
+import * as moment from "moment"
+import "rxjs/add/observable/interval"
+import { Observable } from "rxjs/Observable"
 
 declare let kongregateAPI
 declare let reload
 
 @Injectable()
 export class GameService {
-
-  game: GameModel;
-  last: number;
-  selectedGen: string;
+  game: GameModel
+  last: number
+  selectedGen: string
   list: any
   interval = 1000 / 10
   saveFreq = 1000 * 3 * 60
@@ -29,10 +28,7 @@ export class GameService {
   kongregate: any
   isMainNav = true
 
-  constructor(
-    private router: Router,
-    public toastr: ToastsManager
-  ) {
+  constructor(private router: Router, public toastr: ToastsManager) {
     this.game = new GameModel()
     this.last = Date.now()
 
@@ -46,9 +42,8 @@ export class GameService {
 
     setInterval(this.save.bind(this), this.saveFreq)
 
-    if (typeof kongregateAPI !== 'undefined') {
+    if (typeof kongregateAPI !== "undefined") {
       kongregateAPI.loadAPI(() => {
-
         this.kongregate = kongregateAPI.getAPI()
         console.log("KongregateAPI Loaded")
 
@@ -60,18 +55,18 @@ export class GameService {
             this.setSize()
             this.sendKong()
             this.isKongregate = true
-            Observable.interval(this.kongFreq).subscribe(this.sendKong.bind(this))
+            Observable.interval(this.kongFreq).subscribe(
+              this.sendKong.bind(this)
+            )
             // setInterval(this.sendKong.bind(this), this.kongFreq)
           } catch (e) {
             console.log("Error: " + e.message)
           }
         }, 5 * 1000)
-
       })
-    } else
-      console.log("Github build")
+    } else console.log("Github build")
 
-    this.router.navigateByUrl('/main/unit/unit/G1')
+    this.router.navigateByUrl("/main/unit/unit/G1")
   }
 
   update() {
@@ -79,15 +74,16 @@ export class GameService {
     const delta = now - this.last
 
     if (delta > this.interval) {
-      if (delta > 1000)
-        this.game.isChanged = true
+      if (delta > 1000) this.game.isChanged = true
 
       this.game.longUpdate(delta)
 
       this.game.prestige.time.quantity = Decimal.min(
         this.game.prestige.time.quantity.plus(
-          this.game.prestige.timeMaker.quantity.times(0.1).times(delta / 1000)),
-        this.game.prestige.timeBank.quantity.plus(4).times(3600))
+          this.game.prestige.timeMaker.quantity.times(0.1).times(delta / 1000)
+        ),
+        this.game.prestige.timeBank.quantity.plus(4).times(3600)
+      )
 
       this.last = now
     }
@@ -96,13 +92,13 @@ export class GameService {
   }
 
   checkUpgrades() {
-    if (this.isMainNav)
-      this.game.reloadUpIcons()
+    if (this.isMainNav) this.game.reloadUpIcons()
   }
 
   clear() {
     this.game = null
-    localStorage.clear()
+    // localStorage.clear()
+    localStorage.removeItem("save")
     this.game = new GameModel()
     // this.router.navigateByUrl("")
     // window.location.reload(true)
@@ -111,48 +107,53 @@ export class GameService {
 
   save(timer = true) {
     try {
-      if (typeof (Storage) !== 'undefined') {
+      if (typeof Storage !== "undefined") {
         const save = this.game.getSave()
-        localStorage.setItem('save', save)
+        localStorage.setItem("save", save)
         console.log("Saved")
         if (!timer || !this.game.hideSaveNotification)
-          this.toastr.success("", 'Game Saved')
+          this.toastr.success("", "Game Saved")
       } else {
         this.toastr.warning("Canot access local storage", "Not saved")
       }
     } catch (ex) {
-      this.toastr.error(ex && ex.message ? ex.message : "unknow error", "Saving Error")
+      this.toastr.error(
+        ex && ex.message ? ex.message : "unknow error",
+        "Saving Error"
+      )
     }
   }
 
   load(notify = true, first = false): number {
     try {
-      if (typeof (Storage) !== 'undefined') {
-        const saveRaw = localStorage.getItem('save')
+      if (typeof Storage !== "undefined") {
+        const saveRaw = localStorage.getItem("save")
         if (saveRaw) {
           const last = this.game.load(saveRaw)
 
           if (last) {
             if (notify)
-              this.toastr.success("Idle time: " + moment.duration(Date.now() - last).humanize(), "Game Loaded")
+              this.toastr.success(
+                "Idle time: " + moment.duration(Date.now() - last).humanize(),
+                "Game Loaded"
+              )
             this.last = last
             return last
           } else {
             this.toastr.error("Cannot read your savegame", "Error")
           }
-
         } else {
-          if (!first)
-            this.toastr.error("No savegame found", "Error")
+          if (!first) this.toastr.error("No savegame found", "Error")
         }
-
       } else {
         this.toastr.warning("Cannot access localstorage", "Not Loaded")
       }
     } catch (ex) {
-      this.toastr.error(ex && ex.message ? ex.message : "unknow error", "Load Error")
+      this.toastr.error(
+        ex && ex.message ? ex.message : "unknow error",
+        "Load Error"
+      )
     }
-
   }
 
   nonInfinite(num: decimal.Decimal): number {
@@ -162,18 +163,28 @@ export class GameService {
 
   sendKong() {
     try {
-      this.kongregate.stats.submit('Prestige', this.nonInfinite(this.game.maxLevel))
-      this.kongregate.stats.submit('Prestige2', this.nonInfinite(this.game.maxLevel))
-      this.kongregate.stats.submit('World Done', this.nonInfinite(this.game.prestigeDone))
+      this.kongregate.stats.submit(
+        "Prestige",
+        this.nonInfinite(this.game.maxLevel)
+      )
+      this.kongregate.stats.submit(
+        "Prestige2",
+        this.nonInfinite(this.game.maxLevel)
+      )
+      this.kongregate.stats.submit(
+        "World Done",
+        this.nonInfinite(this.game.prestigeDone)
+      )
       console.log("Prestige sent: " + this.game.maxLevel.toNumber())
     } catch (e) {
       console.log("Error: " + e.message)
     }
-
   }
   setSize() {
     if (this.game.options.width > 1050 && this.game.options.height > 700)
-      this.kongregate.services.resizeGame(this.game.options.width, this.game.options.height)
-
+      this.kongregate.services.resizeGame(
+        this.game.options.width,
+        this.game.options.height
+      )
   }
 }
